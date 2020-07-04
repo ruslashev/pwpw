9c8ee5cb7311ca775b3bdd65de4a240eb295805c
Ruslan Akbashev <ruslashev@gmail.com>
2020-07-05 02:17:24 +0300

    mvp matrix

--- renderer.cc
+++ renderer.cc
@@ -2,7 +2,10 @@
 #include <GL/glew.h>
 
 #include "renderer.hh"
-#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void renderer::init(int w, int h)
 {
@@ -40,9 +43,13 @@ void renderer::init(int w, int h)
 
 		in vec2 position;
 
+		uniform mat4 model;
+		uniform mat4 view;
+		uniform mat4 proj;
+
 		void main()
 		{
-			gl_Position = vec4(position, 0.0, 1.0);
+			gl_Position = proj * view * model * vec4(position, 0.0, 1.0);
 		}
 	)";
 
@@ -79,6 +86,20 @@ void renderer::init(int w, int h)
 	glVertexAttribPointer(pos_attrib_id, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
 	glEnableVertexAttribArray(pos_attrib_id);
+
+	glm::mat4 model = glm::mat4(1.0f);
+	GLint uni_trans_id = glGetUniformLocation(shprog_id, "model");
+	glUniformMatrix4fv(uni_trans_id, 1, GL_FALSE, glm::value_ptr(model));
+
+	glm::mat4 view = glm::lookAt(glm::vec3(1.f, 1.f, 1.f),
+	                             glm::vec3(0.f, 0.f, 0.f),
+	                             glm::vec3(0.f, 0.f, 1.f));
+	GLint uni_view_id = glGetUniformLocation(shprog_id, "view");
+	glUniformMatrix4fv(uni_view_id, 1, GL_FALSE, glm::value_ptr(view));
+
+	glm::mat4 proj = glm::perspective(glm::radians(60.0f), (float)w / (float)h, 0.1f, 10.0f);
+	GLint uni_proj_id = glGetUniformLocation(shprog_id, "proj");
+	glUniformMatrix4fv(uni_proj_id, 1, GL_FALSE, glm::value_ptr(proj));
 }
 
 void renderer::render()
