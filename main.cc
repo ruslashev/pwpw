2099aa1309d4f1ab05713cfd9e6bc73f6e5d14df
Ruslan Akbashev <ruslashev@gmail.com>
2020-07-05 22:21:14 +0300

    add a smarter main loop

--- main.cc
+++ main.cc
@@ -5,22 +5,40 @@ int main()
 {
 	wm w;
 	renderer r;
-	int winw = 1280, winh = (winw * 3) / 4;
+	const int winw = 1280, winh = (winw * 3) / 4;
 
 	w.init(winw, winh);
 	r.init(winw, winh);
 
+	const int tickrate = 50;
+	const double dt = 1. / (double)tickrate;
+
+	double previous = w.get_time();
+	double lag = 0.0;
+
 	bool done = false;
 
 	while (!done) {
-		w.poll_events();
+		double current = w.get_time();
+		double elapsed = current - previous;
 
-		if (w.key_down(KEY_ESC))
-			done = true;
+		previous = current;
+		lag += elapsed;
 
-		done |= w.should_close();
+		while (lag >= dt) {
+			w.poll_events();
+
+			if (w.key_down(KEY_ESC))
+				done = true;
+
+			// update();
+
+			lag -= dt;
+		}
 
 		r.render();
+
+		done |= w.should_close();
 
 		w.swap_window();
 	}
